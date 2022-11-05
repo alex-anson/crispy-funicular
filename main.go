ae212ff96bd7a2fbe9d593f0ef03385c12038bce
Alex Anson <1alex.anson@gmail.com>
2022-11-05 18:01:44 -0400

    organizational switches :shrug:

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
 	"log"
 	"net/http"
 
+	// Using the gorilla/mux 3rd party router package instead of the standard library
+	// net/http router. Allows you to more easily perform tasks such as parsing path
+	// or query params.
 	"github.com/gorilla/mux"
 )
 
-// Using the gorilla/mux 3rd party router package instead of the standard library
-// net/http router. Allows you to more easily perform tasks such as parsing path
-// or query params.
+const PORT = ":10000"
 
 type Movie struct {
 	Id          string `json:"Id"`
@@ -24,18 +25,11 @@ type Movie struct {
 // Global Movies array. Can populate in the `main` function to simulate a db
 var MovieList []Movie
 
-const allAtOnceDesc = "When an interdimensional rupture unravels reality, an unlikely hero must channel her newfound powers to fight bizarre and bewildering dangers from the multiverse as the fate of the world hangs in the balance."
-const troopersDesc = "Five Vermont state troopers, avid pranksters with a knack for screwing up, try to save their jobs and out-do the local police department by solving a crime."
+// Descriptions
+const allAtOnceDesc = "When an interdimensional rupture unravels reality, an unlikely hero must channel her newfound powers to fight bizarre and..."
+const troopersDesc = "Five Vermont state troopers, avid pranksters with a knack for screwing up, try to save their jobs and out-do the local police..."
 
-// Handles requests to the root URL.
-func homePage(w http.ResponseWriter, r *http.Request) {
-	// func Fprintf(w io.Writer, format string, a ...any) (n int, err error)
-	// Fprintf formats according to a format specifier and writes to w. It returns
-	// the number of bytes written and any write error encountered.
-	fmt.Fprintf(w, "Welcome to the homepage of my first Go repo 🂡")
-	fmt.Println("Endpoint hit: homepage")
-}
-
+// IMPORTANT: REGISTER HANDLERS HERE
 // Matches the URL path hit with a defined function
 func handleRequests() {
 	// Create a new instance of a mux router
@@ -48,7 +42,7 @@ func handleRequests() {
 	// {id} = path variable
 	myRouter.HandleFunc("/movie/{id}", getMovie)
 
-	log.Fatal(http.ListenAndServe(":10000", myRouter))
+	log.Fatal(http.ListenAndServe(PORT, myRouter))
 }
 
 // Obvi most important ƒn ✨
@@ -60,6 +54,14 @@ func main() {
 		{Id: "2", Title: "Super Troopers", Desc: troopersDesc, ReleaseYear: 2001},
 	}
 	handleRequests()
+}
+
+// SECTION: Route Handlers
+
+// Handles requests to the root URL.
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the homepage of my first Go repo 🂡")
+	fmt.Println("Endpoint hit: homepage")
 }
 
 func getMovieList(w http.ResponseWriter, r *http.Request) {
@@ -86,6 +88,8 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 // the "C" of CRUD
 // ... doesn't take validation into consideration.
 func addMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint hit (POST request received): /movie")
+
 	postBody, err := io.ReadAll(r.Body)
 
 	if err != nil {
