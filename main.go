1fc446d5db71a27b7bac60a8e9dbfb8b76a45e1b
Alex Anson <1alex.anson@gmail.com>
2022-11-07 11:35:35 -0500

    delete endpoint

--- main.go
+++ main.go
@@ -22,6 +22,9 @@ type Movie struct {
 	ReleaseYear int    `json:"ReleaseYear"`
 }
 
+// NOTE: updating a global variable (MovieList) in order to keep this simple. Not
+// doing any checks to ensure race conditions don't happen. This code isn't "thread-safe".
+
 // Global Movies array. Can populate in the `main` function to simulate a db
 var MovieList []Movie
 
@@ -29,9 +32,9 @@ var MovieList []Movie
 const allAtOnceDesc = "When an interdimensional rupture unravels reality, an unlikely hero must channel her newfound powers to fight bizarre and..."
 const troopersDesc = "Five Vermont state troopers, avid pranksters with a knack for screwing up, try to save their jobs and out-do the local police..."
 
-// IMPORTANT: REGISTER HANDLERS HERE
+// IMPORTANT:
 // Matches the URL path hit with a defined function
-func handleRequests() {
+func registerHandlers() {
 	// Create a new instance of a mux router
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
@@ -39,6 +42,8 @@ func handleRequests() {
 	myRouter.HandleFunc("/movies", getMovieList)
 	// NOTE: Order matters. Must be before the other "/movie" endpoint
 	myRouter.HandleFunc("/movie", addMovie).Methods("POST")
+	// Order still matters.
+	myRouter.HandleFunc("/movie/{id}", deleteMovie).Methods("DELETE")
 	// {id} = path variable
 	myRouter.HandleFunc("/movie/{id}", getMovie)
 
@@ -53,7 +58,7 @@ func main() {
 		{Id: "1", Title: "Everything Everywhere All at Once", Desc: allAtOnceDesc, ReleaseYear: 2022},
 		{Id: "2", Title: "Super Troopers", Desc: troopersDesc, ReleaseYear: 2001},
 	}
-	handleRequests()
+	registerHandlers()
 }
 
 // SECTION: Route Handlers
@@ -64,12 +69,14 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint hit: homepage")
 }
 
+// "R" of CRUD
 func getMovieList(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint hit: /movies")
 	// Encodes the movies into a JSON string
 	json.NewEncoder(w).Encode(MovieList)
 }
 
+// "R" of CRUD
 func getMovie(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint hit: /movie/{id}")
 
@@ -110,4 +117,20 @@ func addMovie(w http.ResponseWriter, r *http.Request) {
 	// %+v  -> value in a default format, with field name.
 	// use when printing structs
 	fmt.Fprintf(w, "%+v", string(postBody))
+}
+
+// the "D" of CRUD
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint hit (DELETE request received): /movie/{id}")
+
+	// Extract the ID
+	routeVariables := mux.Vars(r)
+	deleteId := routeVariables["id"]
+
+	// Loop the movies, remove any entry whose Id property matches deleteId
+	for index, movie := range MovieList {
+		if movie.Id == deleteId {
+			MovieList = append(MovieList[:index], MovieList[index+1:]...)
+		}
+	}
 }
